72ff2a01b9b73c171753b7b5a0fc4474b2adcf64
Michael Lai <themichaellai@gmail.com>
2013-03-13 13:45:52 -0400

    Notification of whether courses were added or not

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,7 @@
 require 'student_info'
+
 class CoursesController < ApplicationController
+  include ActionView::Helpers::TextHelper
   before_filter :authenticate_user!, only: [:aces, :aces_link]
 
   def aces
@@ -12,11 +14,19 @@ class CoursesController < ApplicationController
       flash[:error] = 'Wrong credentials/Not enrolled in any classes!'
       redirect_to aces_link_courses_path
     else
+      found = 0
       aces_res[:schedule].each do |course_data|
-        course = Course.find_or_create_by_number(course_data[:number], name: course_data[:number])
+        course = Course.find_or_create_by_number(course_data[:number]) do |course|
+          course.name = course_data[:number]
+          found += 1
+        end
         current_user.courses << course
       end
-      flash[:success] = 'Courses in ACES Linked!'
+      if found > 0
+        flash[:success] = "#{pluralize(found, 'course')} in ACES linked!"
+      else
+        flash[:notice] = "You already belong to all your courses in ACES!"
+      end
       redirect_to user_path(current_user)
     end
   end
