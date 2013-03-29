5cf0a280ba7b319fe14bea6068781019458892d0
Michael Lai <themichaellai@gmail.com>
2013-03-29 17:47:33 -0400

    Editing of notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,6 @@
 class NotesController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create]
+  before_filter :correct_user, only: [:edit, :update]
 
   def new
     @course = Course.find params[:course_id]
@@ -24,4 +25,31 @@ class NotesController < ApplicationController
   def show
     @note = Note.find params[:id]
   end
+
+  def edit
+    @course = Course.find params[:course_id]
+    # allows user to keep adding a bunch of attachments, is this cool?
+    5.times {@note.attachments.build}
+  end
+
+  def update
+    @note.update_attributes params[:note]
+    if @note.save
+      flash[:success] = 'Note updated!'
+      redirect_to course_note_path(@note.course, @note)
+    else
+      flash[:error] = 'Note is invalid!'
+      render 'edit'
+    end
+  end
+
+  private
+
+    def correct_user
+      @note = Note.find params[:id]
+      unless @note.user == current_user
+        flash[:error] = 'You cannot edit this note!'
+        redirect_to course_note_path(@note.course, @note)
+      end
+    end
 end
