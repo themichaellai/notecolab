3ec7d832d1b58954bd0aa387bd2aa376c826ec62
Michael Lai <themichaellai@gmail.com>
2013-04-08 23:49:46 -0400

    gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,11 @@ module ApplicationHelper
       "#{page_title} | #{base_title}"
     end
   end
+
+  def gravatar_for(user, options)
+    width = options[:width] || 80
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag gravatar_url, alt: user.name, class: 'gravatar', width: width
+  end
 end
